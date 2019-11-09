1759e61d3f609976a99d44ea06debc41c5e70f84
ahmarulabdi <rndmjck@gmai.com>
2019-11-09 18:27:58 +0700

    searching finish with rxjs finished

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { Hero } from '../hero';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-search',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-search.component.css']
 })
 export class HeroSearchComponent implements OnInit {
+  heroes$: Observable<Hero[]>;
+  private searchTerm = new Subject<string>();
+  constructor(
+    private heroService: HeroService
+  ) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerm.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term:string) => this.heroService.searchHeroes(term))
+    )
   }
+
+  search(term: string){
+    this.searchTerm.next(term);
+  }
+
+
+
 
 }
